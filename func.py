0d97ef64ec9cfea27b224debdde1f71e4a37d90a
y.yan.l.liu@oracle.com <y.yan.l.liu@oracle.com>
2023-05-19 06:35:28 +0000

    Changes-Hello-to-Greetings

--- func.py
+++ func.py
@@ -16,6 +16,6 @@ def handler(ctx, data: io.BytesIO = None):
     logging.getLogger().info("Inside Python Hello World function")
     return response.Response(
         ctx, response_data=json.dumps(
-            {"message": "Hello {0}".format(name)}),
+            {"message": "Greetings {0}".format(name)}),
         headers={"Content-Type": "application/json"}
     )
